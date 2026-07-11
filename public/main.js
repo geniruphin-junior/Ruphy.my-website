f4adf0ad4e0779000ffe26145d206bf8e6418581
geniruphin-junior <geniruphin@gmail.com>
2026-07-11 18:35:33 +0200

    add:remove somes functions

--- public/main.js
+++ public/main.js
@@ -12,32 +12,6 @@ function speak(text) {
   synth.speak(utter);
 }
 
-function simulerCompilation() {
-  speak(
-    "Initialisation du compilateur C plus plus... Vérification des dépendances... Compilation réussie.",
-  );
-  alert("🚀 Code C++ compilé avec succès (Simulation)");
-}
-function demarrerJava() {
-  // 1. Message vocal pro
-  speak(
-    "Démarrage de la machine virtuelle Java. Chargement du Bytecode Ruphy.",
-  );
-
-  // 2. Simulation technique dans la console (Appuie sur F12 pour voir)
-  console.log(" [SYSTEM] Initializing JVM...");
-  console.log(" [SYSTEM] Loading: java.lang.Object");
-  console.log(" [SYSTEM] Loading: ruphy.science.Main");
-
-  // 3. Alerte visuelle pour l'utilisateur
-  setTimeout(() => {
-    alert(
-      "☕ [JVM] Java Virtual Machine lancée avec succès.\nCode exécuté en 0.004s",
-    );
-    console.log(" [SUCCESS] Execution finished.");
-  }, 500);
-}
-
 // Effet de console au chargement
 window.onload = () => {
   console.log(
